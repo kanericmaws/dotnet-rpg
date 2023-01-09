e5b59b4bd9cdb030325f2ca80db314a5bdbf840d
kanericmaws <110892824+kanericmaws@users.noreply.github.com>
2023-01-09 10:56:55 -0700

    initial commit

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ namespace dotnet_rpg.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+//test
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
